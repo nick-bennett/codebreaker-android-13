fbc66f8ec51dd4a43416253d14b46cd836b09063
Nick Bennett <nick@nickbenn.com>
2021-07-09 11:50:55 -0600

    Use InputFilter to prevent invalid guesses.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
@@ -23,6 +23,7 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
 
   private final GameRepository repository;
   private final MutableLiveData<Game> game;
+  private final MutableLiveData<String> pool;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
   private final SharedPreferences preferences;
@@ -31,6 +32,7 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     super(application);
     repository = new GameRepository(application);
     game = new MutableLiveData<>();
+    pool = new MutableLiveData<>("ABCDEF");
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
     preferences = PreferenceManager.getDefaultSharedPreferences(application);
@@ -41,6 +43,10 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     return game;
   }
 
+  public LiveData<String> getPool() {
+    return pool;
+  }
+
   public LiveData<Throwable> getThrowable() {
     return throwable;
   }
@@ -49,7 +55,7 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     throwable.setValue(null);
     pending.add(
         repository
-            .create("ABCDEF", getCodeLengthPref())
+            .create(pool.getValue(), getCodeLengthPref())
             .subscribe(
                 game::postValue,
                 this::handleThrowable
