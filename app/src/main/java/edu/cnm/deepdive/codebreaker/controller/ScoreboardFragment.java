a6b786e9429207e9973f5652c0ce392956f00165
Nick Bennett <nick@nickbenn.com>
2021-07-16 10:27:03 -0600

    Setting up scoreboard fragment & layout.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoreboardFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoreboardFragment.java
@@ -11,24 +11,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.FragmentScoreboardBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.DashboardViewModel;
+import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
 
 public class ScoreboardFragment extends Fragment {
 
-  private DashboardViewModel dashboardViewModel;
+  private GameViewModel viewModel;
+  private FragmentScoreboardBinding binding;
 
-  public View onCreateView(@NonNull LayoutInflater inflater,
-      ViewGroup container, Bundle savedInstanceState) {
-    dashboardViewModel =
-        new ViewModelProvider(this).get(DashboardViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_scoreboard, container, false);
-    final TextView textView = root.findViewById(R.id.text_dashboard);
-    dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
-    });
-    return root;
+  public View onCreateView(
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    super.onCreateView(inflater, container, savedInstanceState);
+    binding = FragmentScoreboardBinding.inflate(inflater, container, false);
+    // TODO Attach listeners to sliders.
+    return binding.getRoot();
   }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(getActivity()).get(GameViewModel.class);
+    // TODO Add observers.
+  }
+
 }
